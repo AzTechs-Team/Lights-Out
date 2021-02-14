e11137156a66b3bead60bae7da6988aad0c83b2e
nimit2801 <mnimitsavant@outlook.com>
2021-02-14 11:26:24 +0530

    made some stable changes for reaction swipe, embed and crate channel

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const colors = require('colors');
 const axios = require('axios');
-const { Client, ReactionCollector } = require('discord.js');
+const { Client,MessageEmbed, ReactionCollector } = require('discord.js');
 
 const client = new Client({
     partials: ['MESSAGE', 'REACTION']
@@ -14,6 +14,7 @@ const PREFIX = "#";
 const hello = require('./commands/hello');
 const help = require('./commands/help');
 const LightsOn = require('./commands/lightsOn');
+//const getMatches = require('./')    
 
 client.on('ready', () => {
     console.log(`${client.user.username} has logged in!`);
@@ -27,6 +28,7 @@ client.on('ready', () => {
 })
 
 client.on('message', async (message) => {
+    let number = 1;
     if(message.author.bot === true) return;
     if(message.content.startsWith(PREFIX)){
         const [CMD_NAME, ...args] = message.content
@@ -50,9 +52,101 @@ client.on('message', async (message) => {
             help(message);
         }
         break;
-        case "temp" : {
+        case "matches" : {
 
         }
+        break;
+        case "temp" : {
+            //
+        }
+        break;
+        case "getaroom": {
+            let user1  = '642898906115473428';
+            let user2 = '637564801832452097';
+            const server = message.guild;
+            server.channels.create(`Valentine Cafe ${number}`, {reason: 'They needed a room!', type:'text', topic:'Date'})
+            .then(res => {
+                let role_;
+                server.roles.create({
+                    data: {
+                      name: `Super Cute ${number}`,
+                      color: 'RANDOM',
+                    },
+                    reason: 'We need a role for this love birds',
+                  })
+                    .then(role_data => {
+                        role_ = role_data;
+                        try{
+                        user1 = server.members.cache.get(user1);
+                        user2 = server.members.cache.get(user2);
+                        user1.roles.add(role_data);
+                        user2.roles.add(role_data);
+                        res.send(`${role_data}`);
+                        let everyone = server.roles.fetch('744874959905751091');
+                        console.log(everyone);
+                        res.overwritePermissions(everyone, { 'SEND_MESSAGES': false })
+                    }catch(err){console.log(err);}
+                        //res.lockPermissions()
+                        // res.permissionsFor(role_data)
+                        //     .then(perm => {
+                        //     console.log(perm.FLAGS);
+                        // });
+                        console.log('done');
+                    })
+                    .catch(console.error);
+                
+                //res.permissionsFor(role_);
+                number = number + 1;
+            })
+            .catch(console.error);
+        }
+        break;
+        case "dele":{
+            message.channel.delete();
+        }
+        break;
+        case "embeds" : {
+            let info = {
+                name:'Nimit',
+                userdiscordid:'Nimit#4979',
+                age: 19,
+                gender: 'MALE',
+                preferences: 'FEMALE',
+                agerange: '19',
+                bio: 'I\'m alone on valentines',
+                relationship: '',
+                song: 0,
+                pizza: 0,
+                superhero: 0,
+                food: 0,
+                language: 0,
+                philosophy: 0,
+                software: 0
+            };
+            const messageEmbed = new MessageEmbed()
+            .setTitle('Profiles')
+            .addField('Name', `${info.name}`)
+            .addField('bio', `${info.bio}`)
+            .addField('Age', `${info.age}`)
+            .addField('Gender', `${info.gender}`)
+            .setImage('https://cdn.discordapp.com/attachments/809889014995484712/810068490916200508/cbbe702247a3f58cc9a4dc25c6f7c2c1.jpg')
+            message.channel.send(messageEmbed)
+            .then(message_ =>{
+                console.log(message_.id);
+                client.on('messageReactionAdd', function reacto (reaction, user) {
+                    const { name } = reaction.emoji;
+                    const member = reaction.message.guild.members.cache.get(user.id);
+                    console.log(reaction.users);
+                    if('❤️')
+                        if('❤️' || message_.reactions){
+                            client.removeListener('messageReactionAdd', reacto);
+                            console.log('Event Listener for Message Reaction Deleted')
+                            message.channel.send('your like was saved');
+                        }
+                })
+            })
+        }
+        break;
     }
 }})
 
